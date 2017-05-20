95a39b2932fb2dd5adc701e9bc576f83282a0e5d
Alek21 <aleksandarbatin@gmail.com>
2017-05-20 22:56:01 +0200

    test teaser, faq slider - mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,8 +25,8 @@
       $hpSlider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
         //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
         var i = (currentSlide ? currentSlide : 0) + 1;
-        $status.text(i);
-        $total.text('/' + slick.slideCount);
+        $status.text("0"+ i);
+        $total.text('/' + "0" + slick.slideCount);
     });
 
     $('.hpSlider').slick({
